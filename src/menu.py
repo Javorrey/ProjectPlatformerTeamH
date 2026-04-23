eb606c63da24cb80b0306eac04b0a5b5a3778694
Hugo <hugo.flor@edu.uah.es>
2026-04-23 18:18:46 +0200

    Más avances en el menú

--- src/menu.py
+++ src/menu.py
@@ -25,13 +25,13 @@ class MainMenu(arcade.View):
         centro_x = self.window.width / 2
 
         self.boton_jugar.center_x = centro_x
-        self.boton_jugar.center_y = 240
+        self.boton_jugar.center_y = 270
 
         self.boton_nivel.center_x = centro_x
-        self.boton_nivel.center_y = 180
+        self.boton_nivel.center_y = 200
 
         self.boton_ajustes.center_x = centro_x
-        self.boton_ajustes.center_y = 120
+        self.boton_ajustes.center_y = 130
 
         self.boton_salir.center_x = centro_x
         self.boton_salir.center_y = 60
@@ -43,8 +43,42 @@ class MainMenu(arcade.View):
 
         self.lista_botones.draw()
 
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Lógica para detectar clics y cambiar de vista
+        """
+        botones_pulsados = arcade.get_sprites_at_point((x, y), self.lista_botones)
+
+        if len(botones_pulsados) > 0:
+            boton_clicado = botones_pulsados[0]
+
+            #if boton_clicado == self.boton_jugar:
+                #Nueva vista
+            #elif boton_clicado == self.boton_nivel:
+                #Nueva vista
+            #elif boton_clicado == self.boton_ajustes:
+                #Nueva vista
+            if boton_clicado == self.boton_salir:
+                arcade.exit()
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        """
+        Función para añadir efectos cuando se pasa la flecha del ratón por encima de los botones
+        """
+        #Reseteo
+        for boton in self.lista_botones:
+            boton.scale = 1.8
+        
+        #Detección
+        botones_tocados = arcade.get_sprites_at_point((x, y), self.lista_botones)
+
+        #Efecto
+        if len(botones_tocados) > 0:
+            boton_actual = botones_tocados[0]
+            boton_actual.scale = 2.0
+
 if __name__ == '__main__':
-    ventana = arcade.Window(800, 600, "Artemises 67 - Menú Principal")
+    ventana = arcade.Window(800, 600, "Artemis 67 - Menú Principal")
     vista_menu = MainMenu()
     ventana.show_view(vista_menu)
     arcade.run()
