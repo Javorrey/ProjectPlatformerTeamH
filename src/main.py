249a9d50cd5ed90e4db5f0816e8c4c0945a6c80b
dylam <dylam.nolasco@edu.uah.es>
2026-05-30 17:49:38 +0200

    reescalado de disparo principal

--- src/main.py
+++ src/main.py
@@ -358,6 +358,7 @@ class GameView(arcade.View):
         self.player_sprite.change_x_aim = self.velocidad_bala_x 
 
         # ---------------- LOGICA DE DISPARO NORMAL ----------------
+        factor_escala=self.window.width/1280
         if self.can_shoot:
             if self.shoot_pressed:
                 arcade.play_sound(self.shoot_sound)
@@ -376,6 +377,8 @@ class GameView(arcade.View):
                     self.primaryFire_texture_list
                 )
 
+                cts.PRINCIPAL_SHOOT_SCALE= PRINCIPAL_SHOOT_SCALE* factor_escala
+                bullet.scale=cts.PRINCIPAL_SHOOT_SCALE
                 self.scene.add_sprite("Bullets", bullet)
                 self.can_shoot = False
         else:
@@ -401,7 +404,6 @@ class GameView(arcade.View):
                     self,
                     self.secondaryFire_texture_list
                 )
-                factor_escala=self.window.width/1280
                 cts.SECONDARY_SHOOT_SCALE= SECONDARY_SHOOT_SCALE* factor_escala
                 misil.scale=cts.SECONDARY_SHOOT_SCALE
                 
