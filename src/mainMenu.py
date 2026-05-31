44db392262bc9750ee5b753efa7b9880cc5c112b
Javier Ortiz <javor2003@gmail.com>
2026-05-31 23:20:33 +0200

    Case sensitivity name fix

--- src/mainMenu.py
+++ src/mainMenu.py
@@ -18,9 +18,9 @@ class mainMenu(arcade.View):
         """
         super().__init__()
 
-        arcade.load_font(str(BASE_DIR / "assets" / "fonts" / "Fuente_menu.ttf"))
+        arcade.load_font(str(BASE_DIR / "assets" / "fonts" / "fuente_menu.ttf"))
         
-        self.fondo = arcade.load_texture(str(BASE_DIR / "assets" / "images" / "menu" / "main" / "fondo_menu.png"))
+        self.fondo = arcade.load_texture(str(BASE_DIR / "assets" / "images" / "menu" / "main" / "Fondo_menu.png"))
 
         self.lista_botones = arcade.SpriteList()
         self.lista_mando = arcade.SpriteList()
